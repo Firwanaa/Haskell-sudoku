072701679a6bb61a99454dfd286a6a5f700dbe05
firwanaa <firwanaa@sheridancollege.ca>
2022-06-18 19:48:16 -0400

    Basic solver  && fixing some typoes

--- code.hs
+++ code.hs
@@ -6,11 +6,79 @@ type Matrix a = [Row a]
 type Row a = [a]
 type Value = Char
 
+-- breakdown
+-- type Grid = Matrix Value
+--           = [Row Value]
+--           = [[Value]]
+--           = [[Char]
 -- easier way
 -- type Grid = [[Char]]
 
-balnk :: Grid
-blank :: replicate 9 (replicate 9 '.')
+blank :: Grid
+blank = replicate 9 (replicate 9 '.')
+
+-- Solvable only using the basic rules:
+easy                  :: Grid
+easy                  =  ["2....1.38",
+                          "........5",
+                          ".7...6...",
+                          ".......13",
+                          ".981..257",
+                          "31....8..",
+                          "9..8...2.",
+                          ".5..69784",
+                          "4..25...."]
+
+-- First gentle example from sudoku.org.uk:
+
+gentle                :: Grid
+gentle                =  [".1.42...5",
+                          "..2.71.39",
+                          ".......4.",
+                          "2.71....6",
+                          "....4....",
+                          "6....74.3",
+                          ".7.......",
+                          "12.73.5..",
+                          "3...82.7."]
+
+-- First diabolical example:
+
+diabolical            :: Grid
+diabolical            =  [".9.7..86.",
+                          ".31..5.2.",
+                          "8.6......",
+                          "..7.5...6",
+                          "...3.7...",
+                          "5...1.7..",
+                          "......1.9",
+                          ".2.6..35.",
+                          ".54..8.7."]
+
+-- First "unsolvable" (requires backtracking) example:
+
+unsolvable            :: Grid
+unsolvable            =  ["1..9.7..3",
+                          ".8.....7.",
+                          "..9...6..",
+                          "..72.94..",
+                          "41.....95",
+                          "..85.43..",
+                          "..3...7..",
+                          ".5.....4.",
+                          "2..8.6..9"]
+
+-- Minimal sized grid (17 values) with a unique solution:
+minimal               :: Grid
+minimal               =  [".98......",
+                          "....7....",
+                          "....15...",
+                          "1........",
+                          "...2....9",
+                          "...9.6.82",
+                          ".......3.",
+                          "5.1......",
+                          "...4...2."]
 
 boxsize               :: Int
 boxsize               =  3
@@ -53,3 +121,22 @@ valid g               =  all nodups (rows g) &&
 nodups                :: Eq a => [a] -> Bool
 nodups []             =  True
 nodups (x:xs)         =  not (elem x xs) && nodups xs -- nice
+
+type Choices          =  [Value]
+
+choices               :: Grid -> Matrix Choices
+choices               =  map (map choice)
+                         where
+                            choice v = if empty v then values else [v] -- values = ['1'..'9']
+-- choices g = map (map choice) g
+-- cartesian product of list of lists
+cp                    :: [[a]] -> [[a]]
+cp []                 =  [[]]
+cp (xs:xss)           =  [y:ys | y <- xs, ys <- cp xss] -- recursively processing the tail
+
+collapse              :: Matrix [a] -> [Matrix a]
+collapse              =  cp . map cp -- collapse matrix = cp (map cp matrix)
+
+solve :: Grid -> [Grid]
+solve = filter valid . collapse . choices -- using function composition
+-- solve grid = filter valid (collapse (choices g))
